60a294e50a23775b709443a2f66927690b03df22
HManuelCC <al198240@edu.uaa.mx>
2024-04-23 14:43:43 -0600

    Agrgar componentes y crear menu

---
+++ src/app/Opciones/renta/renta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-renta',
+  standalone: true,
+  imports: [],
+  templateUrl: './renta.component.html',
+  styleUrl: './renta.component.css'
+})
+export class RentaComponent {
+
+}
