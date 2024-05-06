ec1c141975bd66d5b610fecff002ceedcd20f19c
JJGlezPerez <josegope@hotmail.com>
2024-05-06 11:19:08 -0600

    video y estilos

--- src/app/Opciones/home/home.component.ts
+++ src/app/Opciones/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { DomseguroPipe } from '../../domseguro.pipe';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DomseguroPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+video: string = "h9dTYG1y21k?si=GRq3sprqebSDFsK3"
 }
